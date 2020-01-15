4d4f4fdbb44575287fd7acce8f87c3cb96a408e4
Rob Zahn <rob.jzahn@gmail.com>
2020-01-15 13:03:49 -0500

    w8d3

---
+++ w8d3/intro_js_problems_01/intro_js.js
@@ -0,0 +1,77 @@
+// function mysteryScoping1() {
+//     console.log("-----");
+//     console.log("mysteryscoping1 \n");
+    
+//     var x = 'out of block';
+//     if (true) {
+//       var x = 'in block';  
+//       console.log(x);
+//     }
+//     console.log(x);
+//     console.log("----- \n");
+//   }
+
+// mysteryScoping1()
+
+// function mysteryScoping2() {
+//     const x = 'out of block';
+    
+//     if (true) {
+//       const x = 'in block';  
+//       console.log(x);
+//     }
+//     console.log(x);
+//   }
+
+// mysteryScoping2()
+
+// function mysteryScoping3() {
+//     const x = 'out of block';
+//     if (true) {
+//       var x = 'in block';  
+//       console.log(x);
+//     }
+//     console.log(x);
+//   }
+
+//   mysteryScoping3()
+
+//   function mysteryScoping4() {
+//     let x = 'out of block';
+//     if (true) {
+//       let x = 'in block';  
+//       console.log(x);
+//     }
+//     console.log(x);
+//   }
+
+//   mysteryScoping4()
+
+// function mysteryScoping5() {
+//     let x = 'out of block';
+    
+//     if (true) {
+//       let x = 'in block';  
+//       console.log(x);
+//     }
+
+//     let x = 'out of block again';
+//     console.log(x);
+//   }
+
+//   mysteryScoping5()
+
+function madLib(verb, adjective, noun) {
+    console.log(`We shall ${verb.toUpperCase()} the ${adjective.toUpperCase()} ${noun.toUpperCase()}.`);
+}
+
+madLib('make', 'best', 'guac');
+
+function isSubstring(searchString, subString) {
+
+    if (true) {
+        
+    } else {
+
+    }
+}
